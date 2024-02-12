a263435c8af2d5b1e0dfbaa1fb6993a3efbfb182
YordanSV <yordan10_pv@hotmail.com>
2024-02-12 10:16:50 -0600

    .

---
+++ src/database_config.php
@@ -0,0 +1,9 @@
+<?php
+// Configuración de la conexión a la base de datos
+$serverName = "LAPTOP-F97SHH9Q\SQLINSTA1";
+$connectionOptions = array(
+    "Database" => "Tienda_Online",
+    "Uid" => "sa",
+    "PWD" => "1234"
+);
+?>
