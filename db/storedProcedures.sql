0bf9ba4937af1e694aadf393a4af5c34b66bf0bd
valeria33802 <ariasvalear@gmail.com>
2024-03-10 20:43:53 -0600

    edited employee to cs

--- db/storedProcedures.sql
+++ db/storedProcedures.sql
@@ -299,186 +299,141 @@ EXEC SP_Register_Clients
 -----------------------------*/
 
 
-Create Procedure SP_Employee_to_Costumer_Update
-@Answer varchar,
-@UserID int,
-@FirstName VARCHAR(50),
-@LastName VARCHAR(50),
-@BirthDate date,
-@Province VARCHAR(50),
-@District VARCHAR(50),
-@Canton VARCHAR(50),
-@NeighBorhood varchar(50),
-@Place_address varchar(255),
-@Phone varchar (20),
-@CardNumber varchar(16),
-@CardType varchar(20),
-@Cs_status varchar(20)
+CREATE PROCEDURE SP_Employee_to_Costumer_Update
+    @Answer VARCHAR(50),
+    @FirstName VARCHAR(50) = NULL,
+    @LastName VARCHAR(50) = NULL,
+    @BirthDate DATE = NULL,
+    @Province VARCHAR(50) = NULL,
+    @District VARCHAR(50) = NULL,
+    @Canton VARCHAR(50) = NULL,
+    @Neighborhood VARCHAR(50) = NULL,
+    @Place_address VARCHAR(255) = NULL,
+    @Phone VARCHAR(20) = NULL,
+    @CardNumber VARCHAR(16) = NULL,
+    @CardType VARCHAR(20) = NULL,
+    @CS_Status VARCHAR(20) = NULL
 AS
-Begin
+BEGIN
+	
+    BEGIN TRY
+	IF 
+       LEN(@FirstName) > 49 OR LEN(@LastName) > 49 OR LEN(@Province) > 49 OR LEN(@District) > 49 OR
+       LEN(@Canton) > 49 OR LEN(@NeighBorhood) > 49 OR LEN(@Place_address) > 254 OR
+       LEN(@Phone) > 19 OR LEN(@CardNumber) > 15 OR LEN(@CardType) > 19
+DECLARE @UserID INT;
+        IF @Answer = '1' AND @FirstName IS NOT NULL
+        BEGIN
+            IF LEN(@FirstName) < 2 OR LEN(@FirstName) > 50
+            BEGIN
+                RAISERROR ('Error: La longitud del nombre debe estar entre 2 y 50 caracteres', 16, 1);
+            END
+            ELSE
+            BEGIN
+                UPDATE Costumers
+                SET FirstName = @FirstName
+                WHERE UserID = @UserID;
+            END
+        END;
+        ELSE IF @Answer = '2' AND @LastName IS NOT NULL
+        BEGIN
+            IF LEN(@LastName) < 2 OR LEN(@LastName) > 50
+            BEGIN
+                RAISERROR ('Error: La longitud del apellido debe estar entre 2 y 50 caracteres', 16, 1);
+            END
+            ELSE
+            BEGIN
+                UPDATE Costumers
+                SET LastName = @LastName
+                WHERE UserID = @UserID;
+            END
+        END;
+        ELSE IF @Answer = '3' AND @BirthDate IS NOT NULL
+        BEGIN
+            UPDATE Costumers
+            SET BirthDate = @BirthDate
+            WHERE UserID = @UserID;
+        END;
+        ELSE IF @Answer = '4' AND @Province IS NOT NULL
+        BEGIN
+            UPDATE Costumers
+            SET Province = @Province
+            WHERE UserID = @UserID;
+        END;
+        ELSE IF @Answer = '5' AND @District IS NOT NULL
+        BEGIN
+            UPDATE Costumers
+            SET District = @District
+            WHERE UserID = @UserID;
+        END;
+        ELSE IF @Answer = '6' AND @Canton IS NOT NULL
+        BEGIN
+            UPDATE Costumers
+            SET Canton = @Canton
+            WHERE UserID = @UserID;
+        END;
+        ELSE IF @Answer = '7' AND @Neighborhood IS NOT NULL
+        BEGIN
+            UPDATE Costumers
+            SET Neighborhood = @Neighborhood
+            WHERE UserID = @UserID;
+        END;
+        ELSE IF @Answer = '8' AND @Place_address IS NOT NULL
+        BEGIN
+            UPDATE Costumers
+            SET Place_address = @Place_address
+            WHERE UserID = @UserID;
+        END;
+       ELSE IF @Answer = '9' AND @Phone IS NOT NULL AND ISNUMERIC(@Phone) = 1 -- Verifica si es un número
+        BEGIN
+            IF EXISTS (SELECT 1 FROM Costumers WHERE Phone = @Phone AND UserID != @UserID)
+            BEGIN
+                RAISERROR ('Error: El número de teléfono ya está en uso por otro usuario', 16, 1);
+            END
+            ELSE
+            BEGIN
+                UPDATE Costumers
+                SET Phone = @Phone
+                WHERE UserID = @UserID;
+            END
+        END;
+        ELSE IF @Answer = '10' AND @CardNumber IS NOT NULL
+        BEGIN
+            UPDATE Costumers
+            SET CardNumber = @CardNumber
+            WHERE UserID = @UserID;
+        END;
+        ELSE IF @Answer = '11' AND @CardType IS NOT NULL
+        BEGIN
+            UPDATE Costumers
+            SET CardType = @CardType
+            WHERE UserID = @UserID;
+        END;
+        ELSE IF @Answer = '12' AND @CS_Status IS NOT NULL
+        BEGIN
+            IF @CS_Status NOT IN ('Active', 'Inactive')
+            BEGIN
+                RAISERROR ('Error: Ingrese un estado válido', 16, 1);
+            END
+            ELSE
+            BEGIN
+                UPDATE Costumers
+                SET Cs_status = @CS_Status
+                WHERE UserID = @UserID;
+            END
+        END;
+        ELSE
+        BEGIN
+            RAISERROR ('Error: Respuesta no válida o falta de datos', 16, 1 );
+        END;
+    END TRY
+    BEGIN CATCH
+        DECLARE @ErrorMessage NVARCHAR(4000);
+        SELECT @ErrorMessage = ERROR_MESSAGE();
+        RAISERROR (@ErrorMessage, 16, 1 );
+    END CATCH;
+END;
 
-/*Si se escoge el numero 1, cambia el nombre*/
-
-If @Answer = '1'
-
-Begin
-
-SELECT UserID from costumers 
-where @FirstName = FirstName;
-
-UPDATE Costumers
-    SET FirstName = @FirstName
-    WHERE UserID = @UserID;
-
-End;
-/*Si se escoge el numero 2, cambia el apellido*/
-
-Else if @Answer = '2'
-Begin
-
-SELECT UserID from costumers 
-where @LastName = LastName;
-
-UPDATE Costumers
-    SET FirstName = @FirstName
-    WHERE UserID = @UserID;
-
-End;
-/*Si se escoge el numero 3, cambia la fecha de nacimiento*/
-
-Else if @Answer = '3'
-Begin
-
-SELECT UserID from costumers 
-where @BirthDate = BirthDate;
-
-UPDATE Costumers
-    SET BirthDate = @BirthDate
-    WHERE UserID = @UserID;
-
-End;
-/*Si se escoge el numero 4, cambia la provincia*/
-Else if @Answer = '4'
-Begin
-
-SELECT UserID from costumers 
-where @Province = Province;
-
-UPDATE Costumers
-    SET @Province = Province
-    WHERE UserID = @UserID;
-End;
-/*Si se escoge el numero 5, cambia el distrito*/
-Else if @Answer = '5'
-Begin
-
-SELECT UserID from costumers 
-where @District = District;
-
-UPDATE Costumers
-    SET @District = District
-    WHERE UserID = @UserID;
-
-End;
-/*Si se escoge el numero 6, cambia el canton*/
-Else if @Answer = '6'
-Begin
-
-SELECT UserID from costumers 
-where @Canton = Canton;
-
-UPDATE Costumers
-    SET @Canton = Canton
-    WHERE UserID = @UserID;
-
-
-End;
-
-/*Si se escoge el numero 7, cambia el barrio*/
-
-Else if @Answer = '7'
-Begin
-SELECT UserID from costumers 
-where @Neighborhood = NeighBorhood;
-
-UPDATE Costumers
-    SET @NeighBorhood = NeighBorhood
-    WHERE UserID = @UserID;
-
-End;
-
-/*Si se escoge el numero 8, cambia la direccion*/
-
-Else if @Answer = '8'
-Begin
-SELECT UserID from costumers 
-where @Place_address = Place_address;
-
-UPDATE Costumers
-    SET @Place_address = Place_address
-    WHERE UserID = @UserID;
-
-	End;
-
-/*Si se escoge el numero 9, cambia el telefono*/
-Else if @Answer = '9'
-Begin
-
-SELECT UserID from costumers 
-where @Phone = Phone;
-
-UPDATE Costumers
-    SET Phone = @Phone
-    WHERE UserID = @UserID;
-
-End;
-
-/*Si se escoge el numero 10, cambia el numero de tarjeta*/
-
-Else if @Answer = '10'
-Begin
-SELECT UserID from costumers 
-where @CardNumber = CardNumber;
-
-UPDATE Costumers
-    SET @CardNumber = Place_address
-    WHERE UserID = @UserID;
-
-End;
-
-/*Si se escoge el numero 11, cambia la el tipo de tarjeta*/
-Else if @Answer = '11'
-Begin
-
-SELECT UserID from costumers 
-where @CardType = CardType;
-
-UPDATE Costumers
-    SET @CardType = CardType
-    WHERE UserID = @UserID;
-
-End;
-
-/*Si se escoge el numero 12, elimina el cliente*/
-
-Else if @Answer = '12'
-Begin
-SELECT UserID from costumers 
-where UserID = @UserID;
-
-UPDATE Costumers
-    SET @Cs_status = cs_status
-    WHERE UserID = @UserID;
-
-	End;
-
-
-Else
-Begin
-Print 'Opción inválida'
-
-End;
-End;
 GO
 /**/
 CREATE PROCEDURE SP_Category_Maintenance
