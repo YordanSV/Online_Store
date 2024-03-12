3464578f2119f2a2162115e139571c8a5a6e4b3d
YordanSV <yordan10_pv@hotmail.com>
2024-03-12 10:36:22 -0600

    Se modifico el trigger calculartotal para hacer conexion con la tabla intermedia

--- db/triggers.sql
+++ db/triggers.sql
@@ -1,8 +1,8 @@
 use Tienda_Online
 
-DROP TRIGGER IF EXISTS Trg_Login_log ON Users;
-DROP TRIGGER IF EXISTS Calcular_totales ON Factura;
-DROP TRIGGER IF EXISTS  TR_Products_CheckInventory On Products;
+DROP TRIGGER IF EXISTS Trg_Login_log
+DROP TRIGGER IF EXISTS Calcular_totales
+DROP TRIGGER IF EXISTS  TR_Products_CheckInventory
 
 /*Trigger
 ------------------
@@ -10,6 +10,7 @@ DROP TRIGGER IF EXISTS  TR_Products_CheckInventory On Products;
 --------------------
 ----------------------
 ------------------------*/
+go
 
 create trigger Trg_Login_log
 ---Aplica a la tabla Users
@@ -73,13 +74,19 @@ BEGIN
     DECLARE @PrecioTotal DECIMAL(10, 2);
 
     -- Calcula el precio total de la factura
-    SELECT @PrecioTotal = SUM(P.Price * F.Quantity)
-    FROM inserted F
-    JOIN Products P ON P.ProductId = F.ProductId;
+    SELECT @PrecioTotal = SUM(P.Price * pxf.Quantity)
+    FROM ProductsXFactura pxf
+    JOIN Products P ON P.ProductId = pxf.ProductId
+	JOIN inserted i on i.FacturaID = pxf.FacturaID;
+
+
+	DECLARE @TotalBruto DECIMAL(10, 2);
+	@TotalBruto = @PrecioTotal
 
     -- Actualiza los campos de total en la tabla factura
     UPDATE Factura
-    SET TotalImpuesto = @PrecioTotal * 0.18, -- Suponiendo un impuesto del 18%
+    SET TotalBruto = @TotalBruto,
+		TotalImpuesto = @PrecioTotal * 0.18, -- Suponiendo un impuesto del 18%
         TotalEnvio = 10, -- Suponiendo un costo de envío fijo de $10
         TotalPagar = @PrecioTotal + (@PrecioTotal * 0.18) + 10 -- Suma del precio total, impuesto y envío
     WHERE FacturaID IN (SELECT FacturaID FROM inserted);
